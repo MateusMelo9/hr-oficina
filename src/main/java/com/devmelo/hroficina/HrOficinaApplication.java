4f65831c2d9fc48283baf6bb1856d9bd11b9f976
mateus.melo9@gmail.com <901223280401ma>
2022-06-14 20:26:37 -0300

    primeiro commit

---
+++ src/main/java/com/devmelo/hroficina/HrOficinaApplication.java
@@ -0,0 +1,13 @@
+package com.devmelo.hroficina;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HrOficinaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HrOficinaApplication.class, args);
+	}
+
+}
